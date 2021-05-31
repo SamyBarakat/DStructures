961a4c45b7e4abb3a97c56dde2dd3883c5ed5eaf
Samy Barakat <sbarakat2@mail.sfsu.edu>
2021-05-30 22:58:22 -0700

    Inserting node at beginning of linked list

---
+++ datastructures.cpp
@@ -0,0 +1,45 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/*
+
+INSERTING A NODE AT THE BEGINNING OF A LINKEDLIST
+
+*/
+
+struct Node {
+int data;
+struct Node *next;
+};
+
+
+struct Node *head; 
+void Insert(int x) {
+Node *temp = (Node*)malloc(sizeof(struct Node)); 
+temp -> data = x;
+temp -> next = head;
+head = temp;
+}
+
+void Print() {
+struct Node *temp = head;
+printf("List is: ");
+while (temp != NULL) {
+    printf(" %d", temp->data);
+    temp = temp -> next;
+    }
+printf("\n");
+}
+
+int main() {
+head = NULL;
+printf("How many numbers?\n");
+int n, i, x;
+scanf("%d", &n);
+for(i = 0; i < n; i++) {
+    printf("Enter the number\n");
+    scanf("%d", &x);
+    Insert(x);
+    Print();
+    }
+}
